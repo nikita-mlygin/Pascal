d603706ddbe8c3e9d4bc29cecab93af396b4d5f6
nikita-mlygin <43859146+nikita-mlygin@users.noreply.github.com>
2021-10-25 23:12:28 +0300

    Add files via upload

---
+++ CountOfChar.pas
@@ -0,0 +1,31 @@
+﻿var s: string;
+i, j, flag, k: integer;
+
+begin
+
+readln (s);
+k:=1;
+
+for i:=2 to length (s) do
+begin
+  flag:=1;
+  
+  if(s[i] <> s[1]) then
+  begin
+    write(s[i]);
+    for j := i+1 to length(s) do
+      if(s[i] = s[j]) then
+      begin
+        flag := flag + 1;
+        s[j] := s[1];
+        k := k-1;
+      end;
+      
+    
+    writeln(': ', flag);
+  end else
+    k += 1;
+end;
+
+writeln(s[1], ': ', k);
+end.
